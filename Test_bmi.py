7d976917fb70b92d597e45d099f4e80d079de7a6
BoonSiong <pohboonsiong540@gmail.com>
2023-10-31 11:35:14 +0800

    Created Unit Test for bmi.py

--- Test_bmi.py
+++ Test_bmi.py
@@ -1,8 +1,23 @@
 import unittest
+import Lab_2.bmi as bmi
 
-class MyTestCase(unittest.TestCase):
-    def test_something(self):
-        self.assertEqual(True, False)  # add assertion here
+print("Test Lab_2")
 
-if __name__ == '__main__':
-    unittest.main()
+
+
+
+def test_bmi_normal_weight():
+    x = 1.78
+    y = 63
+    result = bmi.calculate_bmi(x,y)
+    assert(result == 0)
+def test_bmi_over_weight():
+    x = 1.50
+    y = 90
+    result = bmi.calculate_bmi(x, y)
+    assert (result == 1)
+def test_bmi_under_weight():
+    x = 1.78
+    y = 40
+    result = bmi.calculate_bmi(x, y)
+    assert (result == -1)
